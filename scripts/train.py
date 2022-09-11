9f1588841831818d1153c9a680817fb4fde11744
Yu Yin <yxonic@gmail.com>
2022-09-11 22:54:08 +0800

    feat: basic training framework

--- scripts/train.py
+++ scripts/train.py
@@ -1,5 +1,7 @@
 from argparse import ArgumentParser
 
+from DTransformer.data import KTDataIter
+
 
 # configure main parser
 parser = ArgumentParser()
@@ -8,6 +10,18 @@ parser.add_argument("-c", "--config", help="configuration file in TOML")
 
 def main(args):
     print(args)
+
+    # prepare dataset
+    data = KTDataIter()
+
+    # training
+    for epoch in range(args.n_epochs):
+        for batch in data:
+            train_on_batch(batch)
+
+
+def train_on_batch(batch):
+    pass
 
 
 if __name__ == "__main__":
